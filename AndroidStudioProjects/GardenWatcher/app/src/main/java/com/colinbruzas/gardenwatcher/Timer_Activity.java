877d21cc59b40c6de281af2c0048f10662d69f8b
zeperoni <colinbruzas@gmail.com>
2018-08-27 19:12:05 -0700

    Added indoor and outdoor watering function

--- AndroidStudioProjects/GardenWatcher/app/src/main/java/com/colinbruzas/gardenwatcher/Timer_Activity.java
+++ AndroidStudioProjects/GardenWatcher/app/src/main/java/com/colinbruzas/gardenwatcher/Timer_Activity.java
@@ -15,6 +15,7 @@ import android.widget.TextView;
 public class Timer_Activity extends AppCompatActivity {
     private long startTime = System.currentTimeMillis();
 
+
     protected void onCreate(Bundle savedInstancestate) {
         super.onCreate(savedInstancestate);
         setContentView(R.layout.activity_timer_);
@@ -31,6 +32,22 @@ public class Timer_Activity extends AppCompatActivity {
             public void onClick(View v) {
                Button b = (Button) findViewById(R.id.WaterAll);
                b.setBackground(getDrawable(R.color.lightPurple));
+            }
+        });
+
+        Button outDoorWater = (Button) findViewById(R.id.OutdoorPlant);
+        outDoorWater.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Button b = (Button) findViewById(R.id.OutdoorPlant);
+                b.setBackground(getDrawable(R.color.lightPurple));
+            }
+        });
+
+        Button inDoorWater = (Button) findViewById(R.id.indoorPlant);
+        inDoorWater.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Button b = (Button) findViewById(R.id.indoorPlant);
+                b.setBackground(getDrawable(R.color.lightPurple));
             }
         });
 
